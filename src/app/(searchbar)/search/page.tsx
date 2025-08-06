004e410cc1181c1e1dae79cc01bf777268e8f16c
dev-daeun <devdaeun@gmail.com>
2025-08-06 19:01:10 +0900

    도서 검색 결과 DOM 변경

--- src/app/(searchbar)/search/page.tsx
+++ src/app/(searchbar)/search/page.tsx
@@ -1,6 +1,9 @@
+import BookItemComponent from "../components/book-item";
+import books from "@/mock/books.json"
+
 export default async function Page({ searchParams }: { searchParams: Promise<{ q: string }> }) {
     const { q } = await searchParams;
     return <>
-        <h3>search page {q}</h3>
+        {books.map((book) => <BookItemComponent key={book.id} {...book}/>)}
     </>
 }
