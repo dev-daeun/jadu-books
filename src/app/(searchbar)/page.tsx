d72c53d3c84081d4daa0306b7f65161420b3f72c
dev-daeun <devdaeun@gmail.com>
2025-08-06 18:45:32 +0900

    메인페이지 DOM 변경 및 CSS 적용

--- src/app/(searchbar)/page.tsx
+++ src/app/(searchbar)/page.tsx
@@ -1,7 +1,19 @@
 import styles from "./page.module.css";
+import books from "@/mock/books.json"
 
 export default function Home() {
+  
   return (
-    <div>메인페이지</div>
-  )
+    <div className={styles.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {/* {books.map((book) => <BookLayout key={book.id} {...book}/>)} */}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {/* {books.map((book) => <BookLayout key={book.id} {...book}/>)} */}
+      </section>
+      
+    </div>
+  );
 }
