1ca2072fb163eb470a6cc9b6963825eddcaec933
dev-daeun <devdaeun@gmail.com>
2025-08-07 15:41:59 +0900

    메인페이지, 검색결과 페이지에 백엔드 연동

--- src/app/(searchbar)/page.tsx
+++ src/app/(searchbar)/page.tsx
@@ -1,18 +1,38 @@
+import { fetchAllBooks, fetchRecommendedBooks } from "@/services/books";
 import BookItemComponent from "./components/book-item";
 import styles from "./page.module.css";
-import books from "@/mock/books.json"
 
-export default function Home() {
-  
+
+async function AllBooks(){
+  const books = await fetchAllBooks()
+  return (
+    <div>
+      {books.map((book) => <BookItemComponent key={book.id} {...book}/>)}
+    </div>
+  )
+}
+
+
+async function RecommendedBooks(){
+  const books = await fetchRecommendedBooks()
+  return (
+    <div>
+      {books.map((book) => <BookItemComponent key={book.id} {...book}/>)}
+    </div>
+  )
+}
+
+
+export default async function Home() {
   return (
     <div className={styles.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => <BookItemComponent key={book.id} {...book}/>)}
+        <RecommendedBooks/>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => <BookItemComponent key={book.id} {...book}/>)}
+        <AllBooks/>
       </section>
       
     </div>
--- src/app/(searchbar)/search/page.tsx
+++ src/app/(searchbar)/search/page.tsx
@@ -1,8 +1,10 @@
+import { fetchAllBooks } from "@/services/books";
 import BookItemComponent from "../components/book-item";
-import books from "@/mock/books.json"
+
 
 export default async function Page({ searchParams }: { searchParams: Promise<{ q: string }> }) {
     const { q } = await searchParams;
+    const books = await fetchAllBooks(q)
     return <>
         {books.map((book) => <BookItemComponent key={book.id} {...book}/>)}
     </>
