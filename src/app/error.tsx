c1eb117217a8ff3712629c53353ac1344f97de0b
dev-daeun <devdaeun@gmail.com>
2025-08-09 16:31:35 +0900

    next.js 내에서 에러 발생 시 새로고침 버튼 제공

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,8 +1,21 @@
 "use client"
 
-import { useEffect } from "react"
+import { useRouter } from "next/navigation";
+import { useEffect, startTransition } from "react"
+import styles from "./error.module.css"
 
 export default function Error({ error, reset }: { error: Error, reset: () => void }) {
+    const router = useRouter()
+    const onButtonClick = () => {
+        startTransition(() => {
+            router.refresh()
+            reset()
+        })
+    }
     useEffect(() => { console.error(error) }, [error])
-    return (<p>Next.js 서버에서 오류가 발생했습니다. Next.js 애플리케이션 로그를 확인하세요.</p>)
+    return (
+    <div className={styles.container}>
+        <p>오류가 발생했습니다. 페이지를 새로고침하세요.</p>
+        <button className={styles.refresh_button} onClick={onButtonClick}>새로고침</button>
+    </div>)
 }
