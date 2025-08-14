368fdd6c6a00e38aeb144c160b162a41df9bf1bb
dev-daeun <devdaeun@gmail.com>
2025-08-14 13:21:05 +0900

    인덱스 페이지에 SEO 목적의 메타데이터 추가

--- src/app/(searchbar)/page.tsx
+++ src/app/(searchbar)/page.tsx
@@ -1,4 +1,17 @@
+import { Metadata } from "next";
 import Books from "../books/page";
+
+
+export const metadata: Metadata = {
+  title: "자두북스",
+  description: "자두가 소개하는 도서를 만나보세요",
+  openGraph: {
+    title: "자두북스",
+    description: "자두가 소개하는 도서를 만나보세요",
+    images: ["/thumbnail.png"]
+  }
+}
+
 
 export default function Home() {
   return <Books />
