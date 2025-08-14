b9246750e73084e854fc9dd4965f6197e7ad2fc2
dev-daeun <devdaeun@gmail.com>
2025-08-14 15:16:26 +0900

    도서 상세페이지가 id를 array 타입으로 받는 것을 감안하여 인터셉팅 라우트에서 id를 array 으로 변환하도록 변경

--- src/app/@modal/(.)books/[id]/page.tsx
+++ src/app/@modal/(.)books/[id]/page.tsx
@@ -1,12 +1,16 @@
 // 도서 상세페이지 인터셉팅
- import Modal from "@/app/books/[...id]/components/modal"
+import Modal from "@/app/books/[...id]/components/modal"
 import BookDetail from "@/app/books/[...id]/page"
 
 
 export default function Page(props: any) {
+    const adaptedProps = {
+        ...props,
+        params: { id: [props?.params?.id].filter(Boolean) }
+    }
     return (
         <Modal>
-            <BookDetail {...props} />
+            <BookDetail {...adaptedProps} />
         </Modal>
     )
 }
