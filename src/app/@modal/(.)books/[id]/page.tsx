c1c44013805ec64689479001ba0ec9bd9172e6a0
dev-daeun <devdaeun@gmail.com>
2025-08-14 16:02:59 +0900

    헷갈리는 import 구문 변경

--- src/app/@modal/(.)books/[id]/page.tsx
+++ src/app/@modal/(.)books/[id]/page.tsx
@@ -1,16 +1,16 @@
 // 도서 상세페이지 인터셉팅
 import Modal from "@/app/books/[...id]/components/modal"
-import BookDetail from "@/app/books/[...id]/page"
+import Page from "@/app/books/[...id]/page"
 
 
-export default function Page(props: any) {
+export default function InterceptingPage(props: any) {
     const adaptedProps = {
         ...props,
         params: { id: [props?.params?.id].filter(Boolean) }
     }
     return (
         <Modal>
-            <BookDetail {...adaptedProps} />
+            <Page {...adaptedProps} />
         </Modal>
     )
 }
