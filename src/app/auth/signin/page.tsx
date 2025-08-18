9a540b4f7c3de664af6b02a4a09d6f141d7b3360
dev-daeun <devdaeun@gmail.com>
2025-08-18 19:12:52 +0900

    회원가입 폼에 csrfToken 삽입하도록 변경

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,7 +1,14 @@
 import Link from "next/link";
+import SignIn from "./signin-form";
+import Tokens from "csrf";
+import { getCsrfToken } from "next-auth/react";
+import settings from "@/settings";
 
-export default async function SignIn() {
+
+export default async function Page() {
+    const csrfToken = await getCsrfToken() || new Tokens().create(settings.csrfSecret)
     return <>
+        <SignIn csrfToken={csrfToken} />
         <Link href="/auth/signup">회원가입 하러가기</Link>
     </>
 }
