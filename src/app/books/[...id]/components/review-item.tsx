d12609625976aaa156219bc200f02f9f51da2d00
dev-daeun <devdaeun@gmail.com>
2025-08-18 18:57:08 +0900

    인증된 사용자가 본인이 작성한 리뷰만 삭제할 수 있도록 변경

--- src/app/books/[...id]/components/review-item.tsx
+++ src/app/books/[...id]/components/review-item.tsx
@@ -1,17 +1,30 @@
 import ReviewDelete from "./review-delete"
 import styles  from "./review-item.module.css"
 import { ReviewItem as ReviewItemType } from "@/types/review"
+import { getServerSession } from "next-auth"
 
 
 export default async function ReviewItem({ id, bookId, author, content, createdAt }: ReviewItemType) {
-    return (
-        <section className={styles.container}>
-            <div className={styles.author}>{author}</div>
-            <div className={styles.content}>{content}</div>
-            <div className={styles.bottom_container}>
-                <div>{new Date(createdAt!).toLocaleDateString('sv-SE')}</div>
-                <ReviewDelete reviewId={id} bookId={bookId}/>
-            </div>
+    const session = await getServerSession()
+    if (session?.user?.name === author) {
+        return (
+            <section className={styles.container}>
+                <div className={styles.author}>{author}</div>
+                <div className={styles.content}>{content}</div>
+                <div className={styles.bottom_container}>
+                    <div>{new Date(createdAt!).toLocaleDateString('sv-SE')}</div>
+                    <ReviewDelete reviewId={id} bookId={bookId}/>
+                </div>
+            </section>
+        )
+    } else {
+        return <section className={styles.container}>
+                <div className={styles.author}>{author}</div>
+                <div className={styles.content}>{content}</div>
+                <div className={styles.bottom_container}>
+                    <div>{new Date(createdAt!).toLocaleDateString('sv-SE')}</div>
+                </div>
         </section>
-    )
+    }
+    
 }
