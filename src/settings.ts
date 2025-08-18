92fc374e24a46b69f78b196b024ddeebd425b0e4
dev-daeun <devdaeun@gmail.com>
2025-08-18 16:13:44 +0900

    회원가입/로그인 및 세션 접근에 필요한 시크릿 키 설정

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,7 @@
 const settings = {
-    backendBaseUrl: process.env.BACKEND_BASE_URL
+    backendBaseUrl: process.env.BACKEND_BASE_URL,
+    csrfSecret: process.env.CSRF_SECRET as string,
+    sessionSecret: process.env.NEXTAUTH_SECRET as string,
 } as const;
 
 
